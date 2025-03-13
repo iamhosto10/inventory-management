c228cbade547480eb01bd580b3486bb430932cde
user <gramirez@seeed.us>
2025-03-13 09:51:01 -0500

    fix eslint errors

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -80,8 +80,8 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
-  if (!storeRef.current) {
+  const storeRef = useRef<AppStore | null>(null); // Inicializado con null
+  if (storeRef.current === null) {
     storeRef.current = makeStore();
     setupListeners(storeRef.current.dispatch);
   }
